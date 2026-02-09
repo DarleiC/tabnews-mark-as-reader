309b16fd72924869584bcfd406ecdd26167f5d31
DarleiC <darleicastro87@hotmail.com>
2026-02-09 02:47:27 -0300

    feat: config Termos e padroniza o código com Biome #3

--- main.js
+++ main.js
@@ -1,34 +1,58 @@
 let lastUrl = location.href
 
-function colorLinks() {
-  document.querySelectorAll('address a').forEach(link => {
-      link.style.color = '#3685f3'
-  })
+const regrasTitulos = [
+	{ term: "[PITCH]", color: "#6e7681" },
+	{ term: "IA", color: "#f8b806" },
+	{ term: "AI", color: "#f8b806" },
+	{ term: "SaaS", color: "#f80606" }
+]
 
-  document.querySelectorAll('a').forEach(link => {
-    if (link.textContent?.includes('[PITCH]')) {
-      link.style.color = '#6e7681'
-    }
-    if (link.textContent?.includes('NewsletterOficial')) {
-      link.style.color = '#3fb950'
-    }
-  })
+const regrasAutores = [
+	{ authorDefault: "authorDefault", color: "#3685f3" },
+	{ author: "NewsletterOficial", color: "#3fb950" }
+]
+
+function colorLinks() {
+	document.querySelectorAll("article a").forEach((link) => {
+		const text = link.textContent?.trim()
+
+		if (link.closest("address")) {
+			// Links de autor
+			const regraAutor = regrasAutores.find((r) => text.includes(r.author))
+			if (regraAutor) {
+				link.style.color = regraAutor.color
+			} else {
+				link.style.color = regrasAutores.find((r) => r.authorDefault === "authorDefault").color
+			}
+		} else {
+			// Links de título
+			const regraTitulo = regrasTitulos.find((r) => text.includes(r.term))
+			if (regraTitulo) {
+				link.style.color = regraTitulo.color
+			}
+		}
+	})
 }
 
 function checkUrlChange() {
-  if (location.href !== lastUrl) {
-    lastUrl = location.href
-    colorLinks()
-  }
-  requestAnimationFrame(checkUrlChange)
+	if (location.href !== lastUrl) {
+		lastUrl = location.href
+
+		colorLinks()
+	}
+
+	requestAnimationFrame(checkUrlChange)
 }
 
 colorLinks()
 
 const observer = new MutationObserver(() => {
-  colorLinks()
+	colorLinks()
 })
 
-observer.observe(document.body, { childList: true, subtree: true })
+observer.observe(document.body, {
+	childList: true,
+	subtree: true
+})
 
 checkUrlChange()
